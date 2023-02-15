e55981acb19d171e08b1d68c4b5585f9a6240f8d
evan_sumangkut <rizkyawliafajrin@gmail.com>
2023-02-16 05:31:33 +0700

    update

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = "172.18.20.6";
 $username = "root";
-$password = "";
-$conn = new mysqli($servername, $username, $password,'db_seminar');
+$password = "password";
+$conn = new mysqli($servername, $username, $password,'db');
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
